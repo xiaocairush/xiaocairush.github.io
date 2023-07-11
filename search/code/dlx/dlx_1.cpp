7ce6ead34112db933b7a5946f36ba42395dbf57d
xiaocairush <xiaocairush@gmail.com>
2023-07-12 02:35:54 +0800

    Deployed 556a13cc with MkDocs version: 1.4.3

--- search/code/dlx/dlx_1.cpp
+++ search/code/dlx/dlx_1.cpp
@@ -2,23 +2,23 @@
 const int N = 500 + 10;
 int n, m, idx, ans;
 int first[N], siz[N], stk[N];
-struct DLXNODE {
-  int lc, rc, up, dn, r, c;
-};
-inline int read() {  //快读
-  register int x = 0, f = 0, ch;
+
+int read() {  // 快读
+  int x = 0, f = 0, ch;
   while (!isdigit(ch = getchar())) f |= ch == '-';
   while (isdigit(ch)) x = (x << 1) + (x << 3) + (ch ^ 48), ch = getchar();
   return f ? -x : x;
 }
+
 struct DLX {
   static const int MAXSIZE = 1e5 + 10;
   int n, m, tot, first[MAXSIZE + 10], siz[MAXSIZE + 10];
   int L[MAXSIZE + 10], R[MAXSIZE + 10], U[MAXSIZE + 10], D[MAXSIZE + 10];
   int col[MAXSIZE + 10], row[MAXSIZE + 10];
-  void build(const int &r, const int &c) {  //进行build操作
+
+  void build(const int &r, const int &c) {  // 进行build操作
     n = r, m = c;
-    for (register int i = 0; i <= c; ++i) {
+    for (int i = 0; i <= c; ++i) {
       L[i] = i - 1, R[i] = i + 1;
       U[i] = D[i] = i;
     }
@@ -26,7 +26,8 @@ struct DLX {
     memset(first, 0, sizeof(first));
     memset(siz, 0, sizeof(siz));
   }
-  void insert(const int &r, const int &c) {  //进行insert操作
+
+  void insert(const int &r, const int &c) {  // 进行insert操作
     col[++tot] = c, row[tot] = r, ++siz[c];
     D[tot] = D[c], U[D[c]] = tot, U[tot] = c, D[c] = tot;
     if (!first[r])
@@ -36,25 +37,28 @@ struct DLX {
       L[tot] = first[r], R[first[r]] = tot;
     }
   }
-  void remove(const int &c) {  //进行remove操作
-    register int i, j;
+
+  void remove(const int &c) {  // 进行remove操作
+    int i, j;
     L[R[c]] = L[c], R[L[c]] = R[c];
     for (i = D[c]; i != c; i = D[i])
       for (j = R[i]; j != i; j = R[j])
         U[D[j]] = U[j], D[U[j]] = D[j], --siz[col[j]];
   }
-  void recover(const int &c) {  //进行recover操作
-    register int i, j;
+
+  void recover(const int &c) {  // 进行recover操作
+    int i, j;
     for (i = U[c]; i != c; i = U[i])
       for (j = L[i]; j != i; j = L[j]) U[D[j]] = D[U[j]] = j, ++siz[col[j]];
     L[R[c]] = R[L[c]] = c;
   }
+
   bool dance(int dep) {  // dance
     if (!R[0]) {
       ans = dep;
       return 1;
     }
-    register int i, j, c = R[0];
+    int i, j, c = R[0];
     for (i = R[0]; i != 0; i = R[i])
       if (siz[i] < siz[c]) c = i;
     remove(c);
@@ -68,17 +72,18 @@ struct DLX {
     return 0;
   }
 } solver;
+
 int main() {
   n = read(), m = read();
   solver.build(n, m);
-  for (register int i = 1; i <= n; ++i)
-    for (register int j = 1; j <= m; ++j) {
+  for (int i = 1; i <= n; ++i)
+    for (int j = 1; j <= m; ++j) {
       int x = read();
       if (x) solver.insert(i, j);
     }
   solver.dance(1);
   if (ans)
-    for (register int i = 1; i < ans; ++i) printf("%d ", stk[i]);
+    for (int i = 1; i < ans; ++i) printf("%d ", stk[i]);
   else
     puts("No Solution!");
   return 0;
--- search/code/dlx/dlx_2.cpp
+++ search/code/dlx/dlx_2.cpp
@@ -1,20 +1,23 @@
 #include <bits/stdc++.h>
 const int N = 1e6 + 10;
 int ans[10][10], stk[N];
-inline int read() {
-  register int x = 0, f = 0, ch;
+
+int read() {
+  int x = 0, f = 0, ch;
   while (!isdigit(ch = getchar())) f |= ch == '-';
   while (isdigit(ch)) x = (x << 1) + (x << 3) + (ch ^ 48), ch = getchar();
   return f ? -x : x;
-}  //快读
+}  // 快读
+
 struct DLX {
   static const int MAXSIZE = 1e5 + 10;
   int n, m, tot, first[MAXSIZE + 10], siz[MAXSIZE + 10];
   int L[MAXSIZE + 10], R[MAXSIZE + 10], U[MAXSIZE + 10], D[MAXSIZE + 10];
   int col[MAXSIZE + 10], row[MAXSIZE + 10];
-  void build(const int &r, const int &c) {  //进行build操作
+
+  void build(const int &r, const int &c) {  // 进行build操作
     n = r, m = c;
-    for (register int i = 0; i <= c; ++i) {
+    for (int i = 0; i <= c; ++i) {
       L[i] = i - 1, R[i] = i + 1;
       U[i] = D[i] = i;
     }
@@ -22,7 +25,8 @@ struct DLX {
     memset(first, 0, sizeof(first));
     memset(siz, 0, sizeof(siz));
   }
-  void insert(const int &r, const int &c) {  //进行insert操作
+
+  void insert(const int &r, const int &c) {  // 进行insert操作
     col[++tot] = c, row[tot] = r, ++siz[c];
     D[tot] = D[c], U[D[c]] = tot, U[tot] = c, D[c] = tot;
     if (!first[r])
@@ -32,21 +36,24 @@ struct DLX {
       L[tot] = first[r], R[first[r]] = tot;
     }
   }
-  void remove(const int &c) {  //进行remove操作
-    register int i, j;
+
+  void remove(const int &c) {  // 进行remove操作
+    int i, j;
     L[R[c]] = L[c], R[L[c]] = R[c];
     for (i = D[c]; i != c; i = D[i])
       for (j = R[i]; j != i; j = R[j])
         U[D[j]] = U[j], D[U[j]] = D[j], --siz[col[j]];
   }
-  void recover(const int &c) {  //进行recover操作
-    register int i, j;
+
+  void recover(const int &c) {  // 进行recover操作
+    int i, j;
     for (i = U[c]; i != c; i = U[i])
       for (j = L[i]; j != i; j = L[j]) U[D[j]] = D[U[j]] = j, ++siz[col[j]];
     L[R[c]] = R[L[c]] = c;
   }
+
   bool dance(int dep) {  // dance
-    register int i, j, c = R[0];
+    int i, j, c = R[0];
     if (!R[0]) {
       for (i = 1; i < dep; ++i) {
         int x = (stk[i] - 1) / 9 / 9 + 1;
@@ -69,9 +76,11 @@ struct DLX {
     return 0;
   }
 } solver;
+
 int GetId(int row, int col, int num) {
   return (row - 1) * 9 * 9 + (col - 1) * 9 + num;
 }
+
 void Insert(int row, int col, int num) {
   int dx = (row - 1) / 3 + 1;
   int dy = (col - 1) / 3 + 1;
@@ -86,18 +95,19 @@ void Insert(int row, int col, int num) {
   solver.insert(id, f3);
   solver.insert(id, f4);
 }
+
 int main() {
   solver.build(729, 324);
-  for (register int i = 1; i <= 9; ++i)
-    for (register int j = 1; j <= 9; ++j) {
+  for (int i = 1; i <= 9; ++i)
+    for (int j = 1; j <= 9; ++j) {
       ans[i][j] = read();
-      for (register int v = 1; v <= 9; ++v) {
+      for (int v = 1; v <= 9; ++v) {
         if (ans[i][j] && ans[i][j] != v) continue;
         Insert(i, j, v);
       }
     }
   solver.dance(1);
-  for (register int i = 1; i <= 9; ++i, putchar('\n'))
-    for (register int j = 1; j <= 9; ++j, putchar(' ')) printf("%d", ans[i][j]);
+  for (int i = 1; i <= 9; ++i, putchar('\n'))
+    for (int j = 1; j <= 9; ++j, putchar(' ')) printf("%d", ans[i][j]);
   return 0;
 }
